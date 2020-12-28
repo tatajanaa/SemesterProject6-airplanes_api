f880db39c33ed8f72acd951c41c81fd7e22ff8b8
tatajanaa <37144309+tatajanaa@users.noreply.github.com>
2020-12-28 17:41:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.UseDeveloperExceptionPage();
+
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
